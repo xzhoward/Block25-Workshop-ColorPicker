f84ba72dc3a4b0f47017d9caaefbb652586f3529
Howard Xu <xzhoward6@gmail.com>
2025-03-17 22:47:04 -0400

    Saving local changes before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 // Write your Color component here
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const isSelected = selectedColor === color;
+
+  return (
+    <div
+      className={`${color} ${isSelected ? "selected" : ""}`}
+      onClick={() => setSelectedColor(color)}
+    >
+    </div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="yellow" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="blue" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
